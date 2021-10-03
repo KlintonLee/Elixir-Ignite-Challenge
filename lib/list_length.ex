acaa40689597e586a40865c84b62e2bcad4c696c
Klinton Lee <klintonlee92@gmail.com>
2021-10-03 12:32:37 -0300

    Desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: listLength(list, 0)
+
+  defp listLength([], acc), do: acc
+
+  defp listLength([_ | tail], acc) do
+    acc = acc + 1
+    listLength(tail, acc)
   end
 end
